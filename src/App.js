79daffee761aacd735418e4069694c4d363709f1
Moroj-Alharbi <moroj.alh@gmail.com>
2019-01-17 09:29:31 +0300

    edit merging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import Track from './components/Track';
-<<<<<<< HEAD
 import TrackInfo from './components/TrackInfo';
 import { Button } from 'react-bootstrap';
-=======
-import { Button} from 'react-bootstrap';
 // import TrackInfo from './components/TrackInfo';
->>>>>>> e5fe131f7ed54c2e59a8ca8b66dd8645547bb9d8
+
 
 import './App.css';
 import Main from './components/Main';
@@ -104,12 +101,6 @@ setReqInfo(reqInfo){
           <Button className="track" bsStyle="primary" onClick={() => this.setActivePage('track')}>Track</Button>
           {/* {this.renderTrack()} */}
           </header>
-<<<<<<< HEAD
-          <button bsStyle="primary" onClick={() => this.setActivePage('track')}>Track</button>
-         
-          {/* {this.renderTrack()} */}
-=======
->>>>>>> e5fe131f7ed54c2e59a8ca8b66dd8645547bb9d8
         </div>
         <div>
         {/* {this.renderPage()} */}
